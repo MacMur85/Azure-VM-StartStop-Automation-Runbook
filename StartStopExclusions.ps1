c25c3c787cc737a7157f02d74a146ced4189d6ee
MacMur85 <114145154+MacMur85@users.noreply.github.com>
2023-04-05 10:51:51 +0200

    Timezone change
    Using GMT Standard Time (British time with daylight saving) instead of UTC.

--- StartStopExclusions.ps1
+++ StartStopExclusions.ps1
@@ -53,10 +53,15 @@ function ManageVMPowerState{
     }
 }
 
-$currentTime = (Get-Date).ToUniversalTime()
+$currentTimeUTC = (Get-Date).ToUniversalTime()
 
 Write-Output "Runbook started..."
-Write-Output "Current UTC time: [$($currentTime.ToString("dddd, yyyy MMM dd HH:mm:ss"))] "
+Write-Output "Current UTC time: [$($currentTimeUTC.ToString("dddd, yyyy MMM dd HH:mm:ss"))] "
+
+$currentTime = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($currentTimeUTC, 'GMT Standard Time')
+
+Write-Output "Current GMT Standard Time: [$($currentTime.ToString("dddd, yyyy MMM dd HH:mm:ss"))] "
+
 
 try
 {
